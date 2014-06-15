164206ac3bb221fe84d2a1f81bbf3155600441f7
jmhdez <juan.hernandez.arroyo@gmail.com>
2014-06-15 12:54:26 +0200

    initial config: clojure starter kit + js2-mode

---
+++ init.el
@@ -0,0 +1,13 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("tromey" . "http://tromey.com/elpa/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(load "~/.emacs.d/user.el")
