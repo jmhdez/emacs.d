4cf7e97b117b64bd710b66939c9eab6cb47a6393
jmhdez <juan.hernandez.arroyo@gmail.com>
2014-06-23 10:41:32 +0200

    set Consolas as default font

--- init.el
+++ init.el
@@ -20,9 +20,10 @@
  '(custom-safe-themes (quote ("1ce793cf04c7fbb4648c20f079b687ef10d8ee3014422cf67cf08c92fa6dc77c" default)))
  '(indent-tabs-mode t)
  '(tab-width 4))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#3f3f3f" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "outline" :family "Consolas")))))
