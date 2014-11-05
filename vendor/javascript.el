12e4d9f65b33ef9b299638725ed8bdf743e5b39e
jmhdez <juan.hernandez.arroyo@gmail.com>
2014-11-05 10:46:44 +0100

    replaced jshint with jsxhint to be able to validate jsx files

--- vendor/javascript.el
+++ vendor/javascript.el
@@ -16,8 +16,18 @@
 
 ;; JSHint-style checking
 (require 'flycheck)
+
+(flycheck-define-checker jsxhint-checker
+  "A JSX syntax and style checker based on JSXHint."
+  :command ("jsxhint" source)
+  :error-patterns
+  ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
+  :modes (js2-mode))
+
 (add-hook 'js2-mode-hook
-          (lambda () (flycheck-mode t)))
+          (lambda ()
+			(flycheck-select-checker 'jsxhint-checker)
+			(flycheck-mode t)))
 
 ;; Autocomplete braces
 (add-hook 'js2-mode-hook 'electric-pair-mode)
