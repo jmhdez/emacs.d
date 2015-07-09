90f0f607df7c024a65951b1a16b3f691c6e4aff9
jmhdez <juan.hernandez.arroyo@gmail.com>
2015-07-09 09:57:53 +0200

    disabled flycheck because it has a huge memory leak when using eslint

--- lang/javascript.el
+++ lang/javascript.el
@@ -15,22 +15,22 @@
           (lambda () (js2r-add-keybindings-with-prefix "C-c C-m")))
 
 ;; Real time syntax checking
-(require 'flycheck)
+;;(require 'flycheck)
 
 ;; Remove default jshint flychecker since we are going to use eslint
-(setq-default flycheck-disabled-checkers
-			  (append flycheck-disabled-checkers '(javascript-jshint)))
+;; (setq-default flycheck-disabled-checkers
+;; 			  (append flycheck-disabled-checkers '(javascript-jshint)))
 
-(setq-default flycheck-disabled-checkers
-			  (append flycheck-disabled-checkers '(json-jsonlist)))
+;; (setq-default flycheck-disabled-checkers
+;; 			  (append flycheck-disabled-checkers '(json-jsonlist)))
 
 ;; Add hook to enable eslint
 ;; WARN: libxml2 is required in windows:
 ;; http://sourceforge.net/projects/ezwinports/files/
-(add-hook 'js2-mode-hook
-          (lambda ()
-			(flycheck-select-checker 'javascript-eslint)
-			(flycheck-mode t)))
+;; (add-hook 'js2-mode-hook
+;;           (lambda ()
+;; 			(flycheck-select-checker 'javascript-eslint)
+;; 			(flycheck-mode t)))
 
 
 ;; Autocomplete braces
