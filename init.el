de8d5a552e1a25acd369da7c540e5638663075ae
jmhdez <juan.hernandez.arroyo@gmail.com>
2016-07-07 18:05:20 +0200

    improved typescript support: use always web-mode and flychek on idle

--- init.el
+++ init.el
@@ -130,9 +130,8 @@
 
 ;; company mode for autocompletion
 (add-hook 'after-init-hook 'global-company-mode)
-
-
-
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
 
 ;; Windows hacks.
 
--- lang/typescript.el
+++ lang/typescript.el
@@ -1,33 +1,30 @@
-(add-hook 'typescript-mode-hook
-          (lambda ()
-            (tide-setup)
-            (flycheck-mode +1)
-            (setq flycheck-check-syntax-automatically '(save mode-enabled))
-            (eldoc-mode +1)
-            (company-mode-on)))
-
-;; aligns annotation to the right hand side
-(setq company-tooltip-align-annotations t)
-
-;; formats the buffer before saving
-;;(add-hook 'before-save-hook 'tide-format-before-save)
-
-;; format options
-(setq tide-format-options
-	  '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t	 :placeOpenBraceOnNewLineForFunctions nil :tabSize 4 :convertTabsToSpaces nil))
-;; see https://github.com/Microsoft/TypeScript/blob/cc58e2d7eb144f0b2ff89e6a6685fb4deaa24fde/src/server/protocol.d.ts#L421-473 for the full list available options
-
-;; Tide can be used along with web-mode to edit tsx files
+;; Use web-mode for both ts and tsx files
+;; (I prefer its syntax highlighting instead of typescript-mode)
 (require 'web-mode)
+
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-(add-hook 'web-mode-hook
-          (lambda ()
-            (when (member (file-name-extension buffer-file-name) '("ts" "tsx"))
-              (tide-setup)
-              (flycheck-mode +1)
-              (setq flycheck-check-syntax-automatically '(save mode-enabled))
-              (eldoc-mode +1)
-              (company-mode-on))))
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . web-mode))
+
+(add-hook 'web-mode-hook 'setup-tide-on-web-mode)
+
+(defun setup-tide-on-web-mode ()
+  (when (member (file-name-extension buffer-file-name) '("ts" "tsx"))
+	(tide-setup)
+	(flycheck-mode +1)
+	(setq flycheck-check-syntax-automatically '(save mode-enabled idle-change))
+	(eldoc-mode +1)
+	(company-mode-on)))
+
+
+
+;; Format document before saving with the right options
+;; More options here: https://github.com/Microsoft/TypeScript/blob/cc58e2d7eb144f0b2ff89e6a6685fb4deaa24fde/src/server/protocol.d.ts#L421-473
+
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(setq tide-format-options
+	  '(:tabSize 4
+		:convertTabsToSpaces nil))
 
 
 ;; Keybindings for tide
@@ -36,7 +33,6 @@
   (local-set-key (kbd "C-c C-t r") 'tide-rename-symbol)
   (local-set-key (kbd "C-c C-t s") 'tide-restart-server)
   (local-set-key (kbd "C-c C-t f") 'tide-references))
-
 
 (add-hook 'typescript-mode-hook 'tide-set-keys)
 (add-hook 'web-mode-hook 'tide-set-keys)
