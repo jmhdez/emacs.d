6a08f5ffa33fba46da9bb7ec865eb88a5c9364f9
Juan M. Hernández <juan.hernandez.arroyo@gmail.com>
2016-12-28 14:01:36 +0100

    updated cljs support

--- lang/clojure.el
+++ lang/clojure.el
@@ -28,3 +28,8 @@
 (add-hook 'cider-repl-mode-hook 'auto-complete-mode)
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
 		  
+(require 'cider)
+(setq cider-cljs-lein-repl
+      "(do (require 'figwheel-sidecar.repl-api)
+           (figwheel-sidecar.repl-api/start-figwheel!)
+           (figwheel-sidecar.repl-api/cljs-repl))")
