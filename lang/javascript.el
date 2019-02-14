19b9f63c7f818986479fe8aed84a393e09fc26d8
Juan M. Hernández <juanmah@igt.es>
2019-02-14 16:25:09 +0100

    Using web-mode + eslint for js/jsx files instead of rjsx

--- lang/javascript.el
+++ lang/javascript.el
@@ -2,11 +2,26 @@
 
 ;; major-mode
 (add-to-list 'auto-mode-alist
-             '("\\.js\\'" . rjsx-mode))
+             '("\\.js\\'" . web-mode))
 
-;; Minor modes
-(add-hook 'rjsx-mode-hook 'electric-pair-mode)
-(add-hook 'rjsx-mode-hook 'js2-refactor-mode)
+(add-hook 'web-mode-hook 'setup-js-on-web-mode )
+
+;; Allow flycheck with javascript-eslint in web-mode.
+;; Requires some manual steps:
+;; - Install globally eslint using npm
+;;   npm install -g eslint
+;; - Create a default settings file in %HOME%/.eslintrc.js
+;;   (Sample file is included in this folder)
+(with-eval-after-load 'flycheck
+  (flycheck-add-mode 'javascript-eslint 'web-mode))
+
+(defun setup-js-on-web-mode ()
+  (when (member (file-name-extension (or buffer-file-name "")) '("js" "jsx"))
+	(flycheck-mode)
+	(setq flycheck-check-syntax-automatically '(save mode-enabled idle-change))
+	(setq web-mode-auto-quote-style 2) ; Use single quote
+	(eldoc-mode +1)
+	(company-mode-on)))
 
 ;; Add support for mocha tests
 (setq mocha-command ".\\node_modules\\.bin\\mocha.cmd")
